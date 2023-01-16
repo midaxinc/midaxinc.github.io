558a32f24ebed21495d5a221a673b09bdd6fe56a
Alex R <alex.r@midax.com>
2023-01-16 20:21:18 +0200

    Pub.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {
 "assets/assets/images/record.jpeg": "556686c5027a6e9d77d2bfe7627067d7",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "f46faa7acc7d8bccfa38548f5ad199dd",
+"assets/NOTICES": "a5803a2a404f3c186495cf000f15bb50",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "9eb8a7030c1073cc04ae1667e267e9e2",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -47,11 +47,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e47ba6a5bc7fb10527fd1a4e673f2559",
-"/": "e47ba6a5bc7fb10527fd1a4e673f2559",
-"jsQR.js": "c5749c2591188b6c833dbe21cfd56b04",
-"jsQR.min.js": "d05e0bd4ba291ea8ea7136f907d129b8",
-"main.dart.js": "756ed652eac6496b8c98aea33fe1c30c",
+"index.html": "79d700e93443c1d7c94afac8639d51b6",
+"/": "79d700e93443c1d7c94afac8639d51b6",
+"main.dart.js": "4feb8fef4c14129530ae5ae4832b3503",
 "manifest.json": "b679d6c98d3e48c713bf585221be7b56",
 "version.json": "4186c308404b2739791b46744c44a90a"
 };
