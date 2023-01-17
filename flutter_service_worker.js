e7e43e1387b028b397eb6588161aaadb33a2bdd0
Alex R <alex.r@midax.com>
2023-01-17 23:46:53 +0200

    Pub.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5b0fafc2c01dec0a6ba80c523c4d141c",
-"/": "5b0fafc2c01dec0a6ba80c523c4d141c",
-"main.dart.js": "77e90f3fb55ffdeecbe33032ae5794db",
+"index.html": "a52b7a9daf12ea3e6cabd1f4d516f685",
+"/": "a52b7a9daf12ea3e6cabd1f4d516f685",
+"main.dart.js": "f1dce6f31bc8b2aa3dd4d7921613ed62",
 "manifest.json": "b679d6c98d3e48c713bf585221be7b56",
 "version.json": "4186c308404b2739791b46744c44a90a"
 };
